fb4c64881613c3762319e59c527312df1ee8bb0f
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-10 13:47:58 -0500

    routers

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,9 @@
 // build your `/api/projects` router here
+const express = require("express");
+const db = require(".../data/dbConfig.js")
+
+const router = express.Router()
+
+
+
+module.exports = router;
