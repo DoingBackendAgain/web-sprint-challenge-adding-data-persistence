c4555fe1879fc34f604aa7ce2291a9ff0f84d4f5
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-10 14:32:40 -0500

    fixing?? knexfile??

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@ const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
   migrations: { directory: './data/migrations' },
+  seeds: {directory: "./data/seeds"},
   pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
 }
 
@@ -14,8 +15,5 @@ module.exports = {
   testing: {
     ...sharedConfig,
     connection: { filename: './data/test.db3' },
-  },
-  seeds: {
-    directory: "./data/seeds",
   }
 };
